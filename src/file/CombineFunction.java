f7dfb1275e596ec89f5ed2cf320fc118971267ae
harielZhang <zhang_hariel@126.com>
2018-02-26 18:35:11 +0800

    deeper in recursion

--- src/file/CombineFunction.java
+++ src/file/CombineFunction.java
@@ -22,30 +22,29 @@ public class CombineFunction {
 
     }
 
-    public static void copyFolder(String orifolder){
+    public static void copyFolder(String orifolder, String destfolder){
         File f = new File(orifolder);
-        for (File each : f.listFiles()){
-            if (each.isDirectory())
+        String[] filePath = f.list();
+        if (!(new File(destfolder).exists()))
+            (new File(destfolder)).mkdirs();
 
-            System.out.println(each.getName());
-        }
+        for (int i = 0; i<filePath.length; i++){
+            if ((new File((orifolder + f.separator +filePath[i]))).isDirectory()){
+                copyFolder(orifolder + f.separator + filePath[i], destfolder + f.separator + filePath[i]);
+            }
 
-    }
-
-    private static File detectFiles(File f){
-        int level=0;
-        for (File each : f.listFiles()) {
-            if (f.isDirectory()) {
-                detectFiles(f);
-                level++;
-
+            if (new File(orifolder + f.separator + filePath[i]).isFile()){
+                copyFile(new File(orifolder + f.separator + filePath[i]), new File(destfolder + f.separator + filePath[i]));
             }
         }
 
     }
 
+
+
     public static void main(String[] args) {
         String s1 = "src/testFile";
-        copyFolder(s1);
+        String s2 = "src/ppFile";
+        copyFolder(s1, s2);
     }
 }
