4da2afe3c293b084d0d02482b1d696e9babd05b4
harielZhang <zhang_hariel@126.com>
2018-03-05 12:02:17 +0800

    finally know something about hashcode

--- src/Collection/genericTest.java
+++ src/Collection/genericTest.java
@@ -14,7 +14,7 @@ public class genericTest {
         for (int i = 0 ; i<100; i++){
             hl.add(new Hero(String.valueOf(i+1), 30));
         }
-//        System.out.println(hl.get(6).getName());
+        System.out.println(hl.get(6).getName());
         System.out.println(hl);
         for (int i =0; i<hl.size(); i++){
             if (Integer.parseInt(hl.get(i).getName()) % 8 ==0)
